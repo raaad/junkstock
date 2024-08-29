cb0bcdc03ad27bcc0eca5722f67ea9346a2f1e8d
rad <rad>
2024-08-29 17:43:41 +0300

    eslint custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,10 @@ module.exports = tseslint.config(
     files: ['**/*.ts'],
     extends: [eslint.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic, ...angular.configs.tsRecommended],
     processor: angular.processInlineTemplates,
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: { project: true }
+    },
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
@@ -24,7 +28,34 @@ module.exports = tseslint.config(
           prefix: 'app',
           style: 'kebab-case'
         }
-      ]
+      ],
+      // custom rules
+      'no-console': 'error',
+      'spaced-comment': ['warn', 'always'],
+      'prefer-arrow-callback': 'error',
+
+      '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+      // '@typescript-eslint/no-unnecessary-condition': 'off', // for typeguards & etc
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'default', format: ['camelCase'] },
+        { selector: 'typeLike', format: ['PascalCase'] },
+        { selector: 'enumMember', format: ['PascalCase'] },
+        { selector: ['objectLiteralProperty', 'objectLiteralMethod'], format: ['camelCase', 'PascalCase'] },
+        // "private _value" of property only until https://github.com/typescript-eslint/typescript-eslint/issues/816
+        { selector: 'classProperty', modifiers: ['private'], format: ['camelCase'], leadingUnderscore: 'allow' },
+        // standalone constants
+        { selector: 'variable', modifiers: ['const', 'global'], format: ['camelCase', 'UPPER_CASE'] },
+        // unused parameter
+        { selector: ['variable', 'parameter'], modifiers: ['unused'], format: ['camelCase'], leadingUnderscore: 'allow' }
+      ],
+      '@typescript-eslint/member-ordering': ['warn', { default: { memberTypes: ['field', 'constructor', 'method'] } }],
+      '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public', overrides: { parameterProperties: 'off' } }]
     }
   },
   {
